137100014f1e47ba31e0551044b5380ad6a59157
Jeremy West <jeremy.west@claycountygov.com>
2019-07-10 16:27:39 -0400

    update to include empty controller

---
+++ Controllers/API/DataControl/DataControlController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClayFinancial.Controllers.API.DataControl
+{
+    public class DataControlController : ApiController
+    {
+
+    }
+}
