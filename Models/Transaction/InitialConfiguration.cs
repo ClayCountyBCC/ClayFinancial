e78f1d280a403aa6962c3e13fb1e289a71b5de95
Jeremy West <jeremy.west@claycountygov.com>
2019-07-10 16:21:09 -0400

    bare bones.

--- Models/Transaction/InitialConfiguration.cs
+++ Models/Transaction/InitialConfiguration.cs
@@ -19,5 +19,11 @@ namespace ClayFinancial.Models.Transaction
     public List<Control> controls { get; set; } = new List<Control>();
 
 
+    public List<Department> departments_raw { get; set; } = new List<Department>();
+    public List<PaymentType> payment_types_raw { get; set; } = new List<PaymentType>();
+    public List<Control> controls_raw { get; set; } = new List<Control>();
+
+
+
   }
 }
